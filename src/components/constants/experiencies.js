1813eb9f5cf9e421e42212065e06bfe4e3bc1615
Santiago <infohere52@gmail.com>
2024-03-28 20:01:01 +0100

    feat: Create constants folder

---
+++ src/components/constants/experiencies.js
@@ -0,0 +1,25 @@
+const EXPERIENCES = [
+  {
+    date: "mayo 2022 - actualmente",
+    company: "Eurofirms Group",
+    description:
+      "Desarrollo de la nueva página web de cornerjob. Utilizando ReatcJS, NextJS, typescript con Material UI. Los repositorios se alojan en Azure y para el control de versiones se utilizó Git",
+    position: "Desarrollador Front-end",
+  },
+  {
+    date: "Noviembre de 2021 - mayo de 2022",
+    company: "Músics per la cobla",
+    description:
+      "Desarrolló un Content Management System (CMS). En el lado del Front-end se emplearon JavaScript y ReactJS junto con Material UI. En cuanto al Back-end, se utilizó PHP 8, Symfony 6 y Api Platform. La gestión de datos se llevó a cabo en MySQL. Para la organización y diseño del proyecto se utilizaron Click Up y Whimsical respectivamente. Los repositorios se alojaron en Github y se implementó un control de versiones con Git.",
+    position: "Desarrollador Full-stack",
+  },
+  {
+    date: "Febrero de 2019 - abril de 2019",
+    company: "Umbrella digital partner",
+    description:
+      "Prácticas del ciclo de DAW en las que tenía que mantener y actualizar páginas web junto a sus bases de datos. Desarrollo web en WordPress y PrestaShop.",
+    position: "Desarrollador Front-end",
+  },
+];
+
+export default EXPERIENCES;
---
+++ src/components/constants/projects.js
@@ -0,0 +1,20 @@
+const PROJECTS = [
+  {
+    image: "/yannick-palahi.png",
+    name: "Yannick Palahí",
+    tags: "REACT, REDUX, SCSS, FIREBASE",
+    description:
+      "Portfolio minimalista hecho para un amigo para su proyecto final de carrera.",
+    link: "https://yannick-palahi.web.app",
+  },
+  {
+    image: "/rick-and-morty.png",
+    name: "Rick and Morty",
+    tags: "REACT, REDUX, SCSS",
+    description:
+      "Web hecha en mis inicios en la programación para practicar el uso de una API",
+    link: "https://rickandmortyexample.netlify.app",
+  },
+];
+
+export default PROJECTS;
